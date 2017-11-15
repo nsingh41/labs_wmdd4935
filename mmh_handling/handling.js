c07fed2e3c7779b0e7e689455868a11a60e2041a
Navjot <kanda.navjotsingh@gmail.com>
2017-11-15 05:42:02 +0000

    you commit

---
+++ mmh_handling/handling.js
@@ -0,0 +1,35 @@
+var hapi = require('hapi');
+var Inert = require('inert');
+var Path = require('path');
+
+var server = new hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: __dirname
+            }
+        }
+    }
+});
+
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.register(Inert, (err) => {
+    if (err) throw err;
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+        file: Path.join(__dirname, 'index.html')
+    }
+});
+
+server.start((err) => {
+    if (err) throw err;
+});
