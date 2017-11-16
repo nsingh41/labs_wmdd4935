d57a41d4f079ec7b1544b22c9d4889c165aac87f
Navjot <kanda.navjotsingh@gmail.com>
2017-11-16 20:40:46 +0000

    you commit

---
+++ mmh_helping/helping.js
@@ -0,0 +1,34 @@
+var Hapi = require('hapi');
+var Vision = require('vision');
+var Path = require('path');
+
+var server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.register(Vision, (err) => {
+    if (err) throw err;
+});
+
+server.views({
+    path: Path.join(__dirname, 'templates'),
+    engines: {
+        html: require('handlebars')
+    },
+    helpersPath:  Path.join(__dirname, 'helpers')
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+        view: 'template.html'
+    }
+});
+
+server.start((err) => {
+    if (err) throw err;
+});
